5b0e878b83b51acd96bc0e82d7a5609749abf303
GAVTIN <gauravkumarsinhasinha@gmail.com>
2024-08-01 15:45:55 +0530

    [Product Page][Committing Assets To be Used]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import DisplayCard from './Components/DisplayCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           Learn React
+          <DisplayCard />
       </header>
     </div>
   );
---
+++ src/Components/DisplayCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const DisplayCard = () => {
+  return (
+    <div>
+      Test
+    </div>
+  )
+}
+
+export default DisplayCard
